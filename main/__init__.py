f98525d6a0d5eced68fd371675fa46bbbca29c5e
Sandeep <itissandeep98@gmail.com>
2020-06-22 02:07:16 +0530

    updated files

--- main/__init__.py
+++ main/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
 app = Flask(__name__)
-cors=FLask(app)
+cors=Flask(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 app.secret_key = '5ZN5zi!45QUsGG'
 app.config.from_pyfile('config.cfg')
