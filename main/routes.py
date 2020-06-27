f1c81aca16ba6a91edb0f643ddc3904814b2ac5d
Sandeep <itissandeep98@gmail.com>
2020-06-27 22:11:00 +0530

    username url now shows repo info

--- main/routes.py
+++ main/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template,jsonify
 from main import app
 import pandas as pd
 from flask_cors import cross_origin
@@ -6,35 +6,109 @@ import base64
 import requests
 import json
 
-@app.route('/<username>/<repo>')
-@app.route('/<username>/<repo>/')
-@app.route('/<username>/<repo>/<path:path>')
-def method_name(username,repo,path=""):
-	path=path.split("/")
+server_url = "https://jsonserver-f.herokuapp.com/"
+
+def decode(content):
+	content = base64.b64decode(content)
+	content=content.decode("utf-8")
+	content=content.replace("\t"," ")
+	return content
+
+def fetchrepo(username,repo):
 	url = 'https://api.github.com/repos/'+username+'/'+repo+'/contents/db.json'
 	req = requests.get(url)
+	return req
+
+def fetchUser(username):
+	url = 'https://api.github.com/users/'+username
+	req = requests.get(url)
+	return req
+
+def fetchSocialCircle(username):
+	userList = {"Followers": [], "Following": []}
+	url = "https://api.github.com/users/"+username+"/followers"
+	req = requests.get(url)
+	if req.status_code == requests.codes.ok:
+		req = req.json()
+		for i in req:
+			userList["Followers"].append(server_url+i["login"])
+
+	url = "https://api.github.com/users/"+username+"/following"
+	req = requests.get(url)
+	if req.status_code == requests.codes.ok:
+		req = req.json()
+		for i in req:
+			userList["Following"].append(server_url+i["login"])
+	
+	return userList
+
+
+
+def fetchAllRepo(username):
+	repoList = {"Owned":[],"Starred":[]}
+	url = "https://api.github.com/users/"+username+"/repos"
+	req=requests.get(url)
+	if req.status_code == requests.codes.ok:
+		req=req.json()
+		for i in req:
+			repoList["Owned"].append(server_url+i["full_name"])
+			
+	url = "https://api.github.com/users/"+username+"/starred"
+	req = requests.get(url)
+	if req.status_code == requests.codes.ok:
+		req = req.json()
+		for i in req:
+			repoList["Starred"].append(server_url+i["full_name"])
+	return repoList
+
+
+@app.route('/<username>/')
+@app.route('/<username>')
+def username(username):
+	req=fetchUser(username)
+	if req.status_code == requests.codes.ok:
+		req=req.json()
+		dic = {"Username": req["login"], "Repository List": fetchAllRepo(username),"Social Circle":fetchSocialCircle(username)}
+		return dic
+	else:
+		return{"errmess":"User not found","status":404}
+
+@app.route('/<username>/<repo>')
+@app.route('/<username>/<repo>/')
+def repo(username,repo):
+	req=fetchrepo(username,repo)
 	try:
 		if req.status_code == requests.codes.ok:
 			req = req.json()
-			content = base64.b64decode(req['content'])
-			content=content.decode("utf-8")
-			content=content.replace("\t"," ")
+			content = decode(req['content'])
+			content=json.loads(content)			
+		else:
+			return{"errmess":'db.json file not found', "status":404}
+	except Exception as e:
+		return{"errmess":'Error in fetching the data', "status":404}
+	return content
+
+@app.route('/<username>/<repo>/<path:path>')
+def path(username,repo,path):
+	path=path.split("/")
+	req=fetchrepo(username,repo)
+	try:
+		if req.status_code == requests.codes.ok:
+			req = req.json()
+			content = decode(req['content'])
 			content=json.loads(content)
 			if(path[0]!=""):
 				for i in path:
-					if(type(content)==list):
-						content=content[int(i)]
-					else:
-						content=content[i]
-				content={path[-1]:content}
+					if(i.isnumeric()):
+						i=int(i)
+					content=content[i]
 			
 		else:
-			return{"errmess":'Content was not found', "status":404}
+			return{"errmess":'db.json file not found', "status":404}
 	except Exception as e:
-		return{"errmess":'Content was not found', "status":404}
-		
-
-	return content
+		print(e)
+		return{"errmess":'Error in fetching the data', "status":404}
+	return jsonify(content)
 
 @app.route('/')
 def index():
