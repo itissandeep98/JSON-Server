a971d8cfee3199950de44401efa589a4a17eab9d
Sandeep <itissandeep98@gmail.com>
2020-07-09 17:52:01 +0530

    session added

--- main/routes.py
+++ main/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template,jsonify
+from flask import render_template,jsonify,session
 from main import app
 from main.worker import *
 import json
@@ -7,7 +7,7 @@ import json
 @app.route('/<username>/')
 def username(username):
 	req = fetch('users/'+username)
-	print(req.headers['X-RateLimit-Remaining'])
+	# print(req.headers['X-RateLimit-Remaining'])
 	if req.status_code == requests.codes.ok:
 		req=req.json()
 		info = {"username": req["login"], 
@@ -23,26 +23,31 @@ def username(username):
 
 @app.route('/<username>/followers')
 def followers(username):
-   return jsonify(fetchfollowers(username))
+	return jsonify(fetchfollowers(username))
 
 
 @app.route('/<username>/following')
 def following(username):
-   return jsonify(fetchfollowing(username))
+	return jsonify(fetchfollowing(username))
 
 @app.route('/<username>/starred')
 def starred(username):
-   return jsonify(fetchstarred(username))
+	return jsonify(fetchstarred(username))
 
 @app.route('/<username>/<repo>')
 @app.route('/<username>/<repo>/')
 def repo(username,repo):
-	req = fetch('repos/'+username+'/'+repo+'/contents/'+dbfile)
+	if(username+repo not in session):
+		req = fetch('repos/'+username+'/'+repo+'/contents/'+dbfile)
 	try:
-		if req.status_code == requests.codes.ok:
-			req = req.json()
-			content = decode(req['content'])
-			content=json.loads(content)			
+		if username+repo in session or req.status_code == requests.codes.ok:
+			if (username+repo in session):
+				content = session[username+repo]
+			else:
+				req = req.json()
+				content = decode(req['content'])
+				content = json.loads(content)
+				session[username+repo] = content
 		else:
 			return{"errmess":dbfile+' file not found', "status":404}
 	except Exception as e:
@@ -53,12 +58,18 @@ def repo(username,repo):
 @app.route('/<username>/<repo>/<path:path>')
 def path(username,repo,path):
 	path=path.split("/")
-	req = fetch('repos/'+username+'/'+repo+'/contents/'+dbfile)
+	if(username+repo not in session):
+		req = fetch('repos/'+username+'/'+repo+'/contents/'+dbfile)
 	try:
-		if req.status_code == requests.codes.ok:
-			req = req.json()
-			content = decode(req['content'])
-			content=json.loads(content)
+		if username+repo in session or req.status_code == requests.codes.ok:
+			if (username+repo in session):
+				content=session[username]
+			else:
+				req = req.json()
+				content = decode(req['content'])
+				content=json.loads(content)
+				session[username+repo] = content
+
 			if(path[0]!=""):
 				for i in path:
 					if(i.isnumeric()):
